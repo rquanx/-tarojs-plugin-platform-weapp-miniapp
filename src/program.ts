2437632772ccd8e5d3cff656110241511fadf84d
tanrunquan <tanrunquan@corp.netease.com>
2023-03-02 16:43:34 +0800

    feat: 修正拷贝的projectConfigJson

--- src/program.ts
+++ src/program.ts
@@ -6,4 +6,5 @@ export default class MiniApp extends Weapp {
   platform = 'miniapp';
   runtimePath = `${PACKAGE_NAME}/dist/runtime`;
   taroComponentsPath = `${PACKAGE_NAME}/dist/components-react`;
+  projectConfigJson = 'project.miniapp.json';
 }
